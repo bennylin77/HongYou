5637d55b449f913eb7634f58d19a89687963b3dd
bennylin77 <bennylin77@gmail.com>
2015-12-02 00:04:54 +0800

    update

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,16 @@
+class AdminController < ApplicationController
+  def signIn
+   session[:sign_in]=nil
+   if request.post?
+       if params[:account]=='hongyou29157199' and params[:password]=='2915719929157199'
+         session[:sign_in]='hongyou29157199'
+         #uri=session[:original_uri]
+         #session[:original_uri]=nil
+         redirect_to news_index_path
+       else
+         flash.now[:title]="登入"         
+         flash.now[:notice]="錯誤的帳號或密碼"
+       end
+    end    
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protected
+  def authorize
+    if session[:sign_in].blank?
+      #session[:original_uri]=request.url      
+      flash[:notice]="請先登入"      
+      redirect_to controller: 'admin', action: 'signIn'
+    end
+  end     
 end
